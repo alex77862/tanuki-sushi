d833688101cafc8cb521a7d3c37d895faac5d1fe
Alexandre Tst <alexandre.bretelle71@gmail.com>
2021-11-23 14:12:16 +0100

    first commit

---
+++ frontend/script/vue.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        posts: []
+    },
+    methods: {
+        updatePost() {
+            axios.get('https://jsonplaceholder.typicode.com/posts')
+                .then(reponse => this.posts = reponse.data)
+                .catch(error => console.log(error));
+        },
+        CreerUnPost() {
+            axios.post('https://jsonplaceholder.typicode.com/posts', {
+                    id: 101,
+                    title: 'foo',
+                    body: 'bar',
+                    userId: 1
+                }).then(reponse => console.log(reponse))
+                .catch(error => console.log(error));
+        }
+    }
+});
